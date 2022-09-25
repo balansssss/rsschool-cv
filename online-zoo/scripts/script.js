4a24e911263a2b4f917a8fd8671179da7bc119ce
Balanchuk Sergey <balansstudio1@gmail.com>
2022-09-25 20:03:43 +0300

    feat: add footer.

---
+++ online-zoo/scripts/script.js
@@ -0,0 +1,28 @@
+// Обработка ссылка в футере, для переброски наверх.
+const logo = document.querySelector('#toTop')
+
+logo.addEventListener('click', e => {
+    e.preventDefault()
+    window.scrollTo(0,0)
+})
+
+// Валидатор для input-а с типом email
+const inputEmail = document.querySelector('#iEmail')
+const inputSubmit = document.querySelector('#iSubmit')
+
+function emailValidator(input, submit) {
+    let state = true
+    input.addEventListener('input', () => {
+        const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+        const check = reg.test(input.value)
+        if(check) {
+            submit.className = 'submit'
+            state = false
+        } else if (!check && !state){
+            submit.className = 'submit default'
+            state = true
+        }
+    })
+}
+
+emailValidator(inputEmail, inputSubmit)

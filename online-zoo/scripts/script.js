3148240fb90a22988c610804aa1cc47c38b60073
Balanchuk Sergey <balansstudio1@gmail.com>
2022-10-06 17:41:34 +0300

    feat: add functionality for progressbar on donate page

--- online-zoo/scripts/script.js
+++ online-zoo/scripts/script.js
@@ -22,66 +22,6 @@ function emailValidator(input, submit) {
 
 emailValidator(inputEmail, inputSubmit)
 
-// Функционал для progressbar на странице donate
-if (pageDonate) {
-    // Функции, которые возвращают необходимые элементы (селекторы)
-    function getDivPointChecked() {
-        return document.querySelector('div.point.checked')
-    }
-
-    function getImgChecked(parent) {
-        return parent.querySelector('img.radio_img')
-    }
-
-    function getPointPriceChecked() {
-        return document.querySelector('.donation .progressbar_prices .price.checked')
-    }
-
-    const pointChecked = getDivPointChecked()
-    drawChecked(pointChecked)
-
-    // Функция возвращает div-ы с классом поинт
-    function getDivPoints() {
-        return document.querySelectorAll('div.progressbar div.point')
-    }
-
-    // Заменяем стандартный radio на radio:checked
-    function drawChecked(point) {
-        const prevPointChecked = getDivPointChecked()
-        prevPointChecked.className = 'point'
-        const pointPriceChecked = getPointPriceChecked()
-        if (pointPriceChecked) {
-            pointPriceChecked.className = 'price'
-        }
-        getImgChecked(prevPointChecked).src = '../../assets/img/donate/radio.png'
-
-        point.className = 'point checked'
-        const img = point.querySelector('img.radio_img')
-        img.src = '../../assets/img/donate/radio_checked.png'
-        document.querySelector(`.donation .progressbar_prices .price[data-img='${img.id}']`).className = 'price checked'
-    }
-
-    // Навешиваем событие для всех поинтов на progressbar
-    getDivPoints().forEach(e => {
-        e.addEventListener('click', () => {
-            drawChecked(e)
-        })
-    })
-
-    // Реализация для смены checked эле-та в progressbar
-    window.addEventListener('resize', () => {
-        const windowInnerWidth = window.innerWidth
-        let point = pointChecked
-
-        if (windowInnerWidth < 700) {
-            point = document.querySelector('div.progressbar div.point:nth-child(6)')
-        } else {
-            point = pointChecked
-        }
-        drawChecked(point)
-    })
-}
-
 
 // Переход на страницы по ссылкам в footer
 const footWrapperLinks = document.querySelectorAll('footer .foot .social li')
@@ -102,9 +42,11 @@ const menuWrapper = document.querySelector('#menuWrapper')
 function hideMenu() {
     document.body.style.overflow = 'auto'
     menuWrapper.className = 'menu_wrapper'
+    menuWrapper.style.display = 'none'
 }
 
 butShowMenu.addEventListener('click', () => {
+    menuWrapper.style.display = 'flex'
     document.body.style.overflow = 'hidden'
     menuWrapper.className = 'menu_wrapper active'
 })

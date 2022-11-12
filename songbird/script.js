caa29e567a74910e1ab3a9a4b47468c4eaff2a36
Balanchuk Sergey <balansstudio1@gmail.com>
2022-11-12 08:01:05 +0300

    feat: add js-pagination

--- songbird/script.js
+++ songbird/script.js
@@ -1,5 +1,24 @@
 const btnStartQuiz = document.querySelector('#startQuiz');
 
-btnStartQuiz.addEventListener('click', () => {
-    alert('test');
+const pages = document.querySelectorAll('main.main');
+const pgMain = document.querySelector('main.main-page');
+const pgQuiz = document.querySelector('main.quiz-page');
+
+const nav = document.querySelector('#nav');
+
+function goToPage(page) {
+    pages.forEach(p => {
+        p.classList.remove('active');
+        if (p.classList.value.includes(page)) {
+            p.classList.add('active');
+        }
+    });
+}
+
+nav.addEventListener('click', e => {
+    if (e.target.tagName === 'A') {
+        goToPage(e.target.getAttribute('linkTo'));
+    }
 });
+
+btnStartQuiz.addEventListener('click', () => goToPage('quiz-page'));
